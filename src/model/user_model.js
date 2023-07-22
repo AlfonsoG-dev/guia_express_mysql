5ac262345d63055b9621fee20175cc925196e9b6
tuNovio <alfgomachi@gmail.com>
2023-07-22 18:42:01 -0500

    modelo de la tabla

---
+++ src/model/user_model.js
@@ -0,0 +1,46 @@
+class User {
+    constructor(nombre = '', email = '', password = '', rol = '', create_at = Date) {
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+        this.rol = rol;
+        this.create_at = create_at;
+    }
+
+    //getter
+    get get_name() {
+        return this.nombre
+    }
+    get get_email() {
+        return this.email
+    }
+    get get_password() {
+        return this.password
+    }
+    get get_rol() {
+        return this.rol
+    }
+    get get_create_at() {
+        return this.create_at
+    }
+
+    //setter
+
+    set set_name(nNombre = '') {
+        this.nombre = nNombre
+    }
+    set set_email(nEmail = '') {
+        this.email = nEmail
+    }
+    set set_password(nPassword = '') {
+        this.password = nPassword
+    }
+    set set_rol(nRol = '') {
+        this.rol = nRol
+    }
+    set set_create_at(nCreate_at = Date) {
+        this.create_at = nCreate_at
+    }
+}
+
+module.exports = User
