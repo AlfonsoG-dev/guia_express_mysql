36ec1e8ea4d26b2a5aa9806d280d32d93421c128
tuNovio <alfgomachi@gmail.com>
2023-07-22 18:42:18 -0500

    pruebas de fetch para el server

---
+++ fetch_api.js
@@ -0,0 +1,42 @@
+
+
+function delete_user() {
+
+    return new Promise(function (resolve, reject) {
+        fetch('http://localhost:3000/user/1', {
+            method: 'DELETE',
+        })
+            .then((response) => resolve(response))
+            .catch((err) => reject(err))
+    })
+}
+const date_m = new Date(Date.now())
+const User = {
+    nombre: 'alfonso',
+    email: 'alf@alf',
+    password: '123',
+    rol: 'admin',
+    create_at: date_m
+}
+function create_user() {
+    return new Promise(function (resolve, reject) {
+        fetch('http://localhost:3000/user/post-user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': "user/json",
+            },
+            body: JSON.stringify(User)
+        })
+            .then((result) => result)
+            .then((result) => resolve(result))
+            .catch((err) => reject(err))
+    })
+}
+async function call() {
+    const data = await create_user()
+    console.log(data)
+}
+
+call()
+
+
