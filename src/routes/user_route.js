2978a68b374ab621e04522e5f4c0bd27b43f4269
tuNovio <alfgomachi@gmail.com>
2023-08-12 17:19:11 -0500

    se crea una clase para contener las rutas del usuario

--- src/routes/user_route.js
+++ src/routes/user_route.js
@@ -1,20 +1,33 @@
 //dependencias
-const express = require('express')
-const UserRouterController = require("../controllers/user_route_controller")
-const user_route = express.Router()
+const express = require('express');
+const UserRouterController = require("../controllers/user_route_controller");
+const user_route = express.Router();
 
 
 //instancias 
-const routeController = new UserRouterController()
-user_route.use(routeController.verificar_integridad.bind(routeController))
-user_route.get("/", routeController.list_users.bind(routeController))
+class UserRoutes {
+    constructor() {
+        this.routeController = new UserRouterController();
+    }
 
-user_route.get("/:id", routeController.get_user_id.bind(routeController))
+    get_routes() {
 
-user_route.get("/get-name/:name", routeController.get_user_name.bind(routeController))
+        // TODO:verificar no funciona
+        user_route.use(this.routeController.verificar_integridad_user.bind(this.routeController));
 
-user_route.post("/post-user", routeController.inser_user.bind(routeController))
+        user_route.get("/", this.routeController.list_users.bind(this.routeController));
 
-user_route.delete("/delete-user/:id", routeController.truncate_user.bind(routeController))
+        user_route.get("/:id", this.routeController.get_user_id.bind(this.routeController));
 
-module.exports = user_route
+        user_route.get("/get-name/:name", this.routeController.get_user_name.bind(this.routeController));
+
+        user_route.post("/post-user", this.routeController.insert_user.bind(this.routeController));
+
+        user_route.delete("/delete-user/:id", this.routeController.truncate_user.bind(this.routeController));
+
+        return user_route;
+    }
+
+}
+
+module.exports = UserRoutes;
