72fc762d913389585e60e15beaa455d396378ccb
tuNovio <alfgomachi@gmail.com>
2023-08-12 22:06:38 -0500

    middleware para autenticar; falta agregar que cree la sesión

--- src/middlewares/AutenticateUser.js
+++ src/middlewares/AutenticateUser.js
@@ -1,7 +1,32 @@
-const UserRouterController = require("../controllers/user_route_controller")
+const UserRepository = require("../Repository/UserRepository")
 class AuthMiddleware{
     constructor(){
-        this.controller = new UserRouterController();
+        this.repository = new UserRepository()
+    }
+    async  Login(req, res) {
+        try {
+            const data_req = req.body
+            const data_res = await this.repository.autenticate_user(data_req)
+            if (data_res.length > 0) {
+                /*                req.session.regenerate(function (err) {
+                    if (err) next(err)
+                    req.session.user = data_res[0]
+                    //console.log(req.session.user)
+                    req.session.save(function (err) {
+                        if (err) return next(err)
+                        res.status(200).json({ msg: data_res[0] })
+                    })
+                })
+                 * */
+                res.send("autenticado")
+
+            } else {
+                res.status(400).json({ error: 'usuario incorrecto' })
+            }
+        } catch (error) {
+            throw new Error(error)
+        }
+
     }
 }
 module.exports = AuthMiddleware;
