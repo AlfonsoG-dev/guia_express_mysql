de7ada2ccbdd8110ed422ba4c660e5fd8a20bbb4
tuNovio <alfgomachi@gmail.com>
2023-08-14 10:47:47 -0500

    el middleware se posiciona en la ruta especifica

--- src/ServerAPI.js
+++ src/ServerAPI.js
@@ -40,15 +40,14 @@ class ApiServer {
         this.app.get("/", (req, res) => {
             res.send(req.ip);
         });
-
         this.app.post("/", this.autenticate_user.Login.bind(this.autenticate_user))
 
+        this.UseMidleware()
         this.app.use("/user", this.user_route.get_routes());
     }
 
     init() {
         this.UseLibrery();
-        this.UseMidleware()
         this.UseRoutes();
         this.UseErrorMiddleware();
         this.app.listen(ApiServer.PORT, () => {
